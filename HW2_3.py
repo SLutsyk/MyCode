be5f1efbf6740086518f86af18c0d77597b0c7cb
SLutsyk <solutsyk@gmail.com>
2019-08-08 17:02:57 +0300

    added third task from second homework

---
+++ HW2_3.py
@@ -0,0 +1,9 @@
+a = input("Please enter variable A... ")
+b = input("Please enter variable B... ")
+print()
+print(a, b)
+a, b = b, a
+print()
+print("Swaping variable values... ")
+print()
+print(a, b)
